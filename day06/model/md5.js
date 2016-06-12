f697e5b9308dd97d0d04a8cd2e8feab98a503192
bingoogolapple <bingoogolapple@gmail.com>
2016-06-12 21:35:55 +0800

    添加密码验证

---
+++ day06/model/md5.js
@@ -0,0 +1,9 @@
+/**
+ * Created by bingoogolapple on 16/6/12.
+ */
+var crypto = require("crypto");
+module.exports = function(mingma){
+    var md5 = crypto.createHash('md5');
+    var password = md5.update(mingma).digest('base64');
+    return password;
+}
---
+++ day06/settings.js
@@ -0,0 +1,6 @@
+/**
+ * Created by bingoogolapple on 16/6/12.
+ */
+module.exports = {
+    "dburl" : "mongodb://localhost:27017/day6"
+}
