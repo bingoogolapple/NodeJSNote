8138f0c991b3264f2ff2921921d9dd7b2000e41e
bingoogolapple <bingoogolapple@gmail.com>
2016-06-19 17:59:09 +0800

    「add」添加第七天学习笔记

---
+++ day07/models/db.js
@@ -0,0 +1,13 @@
+/**
+ * Created by bingoogolapple on 16/6/19.
+ */
+//引包
+var mongoose = require('mongoose');
+//创建数据库连接
+var db      = mongoose.createConnection('mongodb://127.0.0.1:27017/haha');
+//监听open事件
+db.once('open', function (callback) {
+    console.log("数据库成功连接");
+});
+//向外暴露这个db对象
+module.exports = db;
